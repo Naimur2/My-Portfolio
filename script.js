222d1f454111439d5a9a6557e2fb62ff386ef3c9
Naimur2 <naimurrahaman248@gmail.com>
2021-03-03 00:04:06 +0600

    changes

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const mobileNav=document.querySelector(".mobile-nav");
 const main=document.querySelector(".main-container");
 const header=document.querySelector(".main-header");
 const navItems=document.querySelectorAll(".main-nav__item");
+const mobileNavItem=document.querySelectorAll(".mobile-nav__item>a");
 
 toggler.addEventListener("click",()=>{
     mobileNav.classList.remove("close");
@@ -13,13 +14,15 @@ toggler.addEventListener("click",()=>{
     backdrop.classList.add("show");
     
 });
-backdrop.addEventListener("click",()=>{
+const hideMobileNav=()=>{
     mobileNav.classList.remove("open");
     mobileNav.classList.add("close");
     backdrop.classList.remove("open-backdrop");
     backdrop.classList.add("close-backdrop");
     setTimeout(()=>backdrop.classList.remove("show"),500);
-    
+}
+backdrop.addEventListener("click",()=>{
+    hideMobileNav();
 });
 let typed = new Typed('.user-qualification', {
     strings: ["Designer.","Freelancer.","Developer."],
@@ -53,9 +56,52 @@ let typed = new Typed('.user-qualification', {
         navItems.forEach((item)=> item.classList.remove("font-change"));
     }
   }
-  
+
+
+
   window.onload=()=>{
     if(window.pageYOffset>=mainTop){
             header.classList.add("back-change__forward");
     }
   }
+  
+
+ const smoothScroll=(targetPosition, duration)=> {
+    const startPosition = window.pageYOffset;
+    const distance = targetPosition - startPosition;
+    let startTime = null;
+
+    function animation(currentTime) {
+        if (startTime == null) startTime = currentTime;
+        let timeElapsed = currentTime - startTime;
+        let run = ease(timeElapsed, startPosition, distance, duration);
+        window.scrollTo(0, run);
+        if (timeElapsed < duration) requestAnimationFrame(animation);
+    }
+
+    function ease(t, b, c, d) {
+        t /= d / 2;
+        if (t < 1) return c / 2 * t * t * t * t + b;
+        t -= 2;
+        return -c / 2 * (t * t * t * t - 2) + b;
+    }
+    hideMobileNav();
+    requestAnimationFrame(animation);
+}
+const srollFunc=(target) => {
+    let elementId = target.getAttribute("href").replace("#","");
+    const targetElement = document.getElementById(elementId);
+    const targetElementPosition = targetElement.getBoundingClientRect().top;
+    target.addEventListener("click", (e) => smoothScroll(targetElementPosition, 1000));
+};
+for(let items of mobileNavItem){
+    items.addEventListener("click",()=>{
+        hideMobileNav();
+        srollFunc(items); 
+        });
+}
+let data = document.querySelectorAll(".smooth-scroll");
+for (let target of data) {
+    srollFunc(target);
+}
+
